7a3f880e46cb4ebd8b34684196c1db4d75ca3819
t-keshi <quick.resp.biz094@gmail.com>
2022-08-27 18:21:41 +0900

    test

--- src/practice3/removeLa.test.ts
+++ src/practice3/removeLa.test.ts
@@ -3,4 +3,5 @@ import { removeLa } from "./removeLa";
 test("removeLaのテスト", () => {
   expect(removeLa("ららら")).toEqual("");
   expect(removeLa("見られる")).toEqual("見れる");
+  expect(removeLa("見ららられる")).toEqual("見れる");
 });
