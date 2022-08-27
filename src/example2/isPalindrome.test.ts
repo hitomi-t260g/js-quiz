c00474ce0bbb8db1d2aa4ab6e2058fbac568f416
t-keshi <quick.resp.biz094@gmail.com>
2022-08-27 18:18:24 +0900

    feature: simple git hooks

--- src/example2/isPalindrome.test.ts
+++ src/example2/isPalindrome.test.ts
@@ -1,5 +1,6 @@
 import { isPalindrome } from "./isPalindrome";
 
 test("isPalindromeのテスト", () => {
-  expect(isPalindrome(101)).toEqual(true);
+  expect(isPalindrome(121)).toEqual(true);
+  expect(isPalindrome(123)).toEqual(false);
 });
